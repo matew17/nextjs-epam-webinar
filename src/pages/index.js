b7aa57d4c91a3fc230d7509a270a123c3a457fc5
Mateo Castaño <matew-17@hotmail.com>
2022-03-06 17:21:33 -0500

    SSR Index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,11 @@
 import Head from "next/head";
+import Image from "next/image";
 
-export default function Home() {
+import styles from "../styles/Home.module.css";
+
+export default function Home({ data }) {
+    const { results } = data;
+
     return (
         <div>
             <Head>
@@ -9,7 +14,37 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <h1>Home page</h1>
+            <h1>Home page - SSR</h1>
+
+            <section className={styles.charatersWrapper}>
+                {results.map((result) => (
+                    <div key={result.id}>
+                        <div className={styles.characterContainer}>
+                            <Image
+                                src={result.image}
+                                alt={result.name}
+                                width={250}
+                                height={250}
+                            />
+                            <p className={styles.name}>{result.name}</p>
+                            <span className={styles.gender}>
+                                {result.gender}
+                            </span>
+                        </div>
+                    </div>
+                ))}
+            </section>
         </div>
     );
+}
+
+export async function getServerSideProps() {
+    const url = "https://rickandmortyapi.com/api/character";
+
+    const response = await fetch(`${url}`);
+    const data = await response.json();
+
+    return {
+        props: { data },
+    };
 }
